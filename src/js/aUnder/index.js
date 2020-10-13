4283f6a26e91e45743a9030b402de3ff511f947a
yuricks7_local <47909257+yuricks7@users.noreply.github.com>
2020-10-13 23:17:23 +0900

    on editing...

--- src/js/aUnder/index.js
+++ src/js/aUnder/index.js
@@ -1,7 +1,7 @@
-// import { main as addModifyDate } from "./modifyDate.js";
+import { main as addModifyDate } from "./modifyDate.js";
 import { main as addTocOnBody } from "./tocOnBody.js";
 import { main as addRowNumbersOnCodeBlocks} from "./rowNumOnCodeBlock.js";
 
-// (addModifyDate($))(jQuery);
+(addModifyDate($))(jQuery);
 addTocOnBody();
 addRowNumbersOnCodeBlocks();
