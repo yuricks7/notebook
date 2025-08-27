90097ea5a1497a1fac84085b608607719da4ebae
20250824_yuricks7 <yuricks7+mousePC@dekoboko-hack.biz>
2025-08-27 12:37:14 +0900

    on testing.

--- src/shared/js/body/entryContent/redirect.js
+++ src/shared/js/body/entryContent/redirect.js
@@ -1,5 +1,8 @@
 /**
  *  リダイレクト
+ *
+ * [note]
+ *  - まだ使い方がよくわかっていない
  *
  * 【参考】
  *  - 記事のURLを変更したときなどに新しい記事URLへリダイレクトする方法 | はてなブログカスタマイズメモ
@@ -8,11 +11,11 @@
  *    https://www.haneda-airport-server.com/entry/hatena-redirect#i
  */
 function redirectTo(url) {
-  setTimeout("redirect()", 3000); // = 3 sec
-
   function redirect() {
     location.href = url;
   }
+
+  setTimeout("redirect()", 3000); // = 3 sec
 }
 
-export { setTimeout, redirectTo };
+export { redirectTo };
